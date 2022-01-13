b0f50f76860fd166e476505f2b54737b16972132
ghizlanerahil <ghizlanerahil@gmail.com>
2022-01-13 10:32:18 +0100

    dao(association,don)

---
+++ src/main/java/com/example/gestion_associations/dao/associationDao.java
@@ -0,0 +1,7 @@
+package com.example.gestion_associations.dao;
+
+import com.example.gestion_associations.bean.association;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface associationDao extends JpaRepository<association,Long>{
+}
---
+++ src/main/java/com/example/gestion_associations/dao/donDao.java
@@ -0,0 +1,8 @@
+package com.example.gestion_associations.dao;
+
+import com.example.gestion_associations.bean.don;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface donDao extends JpaRepository<don,Long>{
+}
