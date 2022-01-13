18bf74b8ad1bfba9d3fb87dcf5c6787551b8d5ca
ghizlanerahil <ghizlanerahil@gmail.com>
2022-01-13 11:27:12 +0100

    Generation des tables

--- src/main/java/com/example/gestion_associations/service/associationService.java
+++ src/main/java/com/example/gestion_associations/service/associationService.java
@@ -1,4 +1,24 @@
 package com.example.gestion_associations.service;
 
+import com.example.gestion_associations.bean.association;
+import com.example.gestion_associations.dao.associationDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class associationService {
+    @Autowired
+    private associationDao associationDao;
+
+    public List<association> findAll() {
+        return associationDao.findAll();
+    }
+
+    public <S extends association> S save(S entity) {
+        return associationDao.save(entity);
+    }
+
+    public void deleteById(Long aLong) {
+        associationDao.deleteById(aLong);
+    }
 }
--- src/main/java/com/example/gestion_associations/service/donService.java
+++ src/main/java/com/example/gestion_associations/service/donService.java
@@ -1,4 +1,28 @@
 package com.example.gestion_associations.service;
 
+import com.example.gestion_associations.bean.don;
+import com.example.gestion_associations.dao.donDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class donService {
+    @Autowired
+    private donDao donDao;
+
+    public List<don> findAll() {
+        return donDao.findAll();
+    }
+
+    public don getById(Long aLong) {
+        return donDao.getById(aLong);
+    }
+
+    public <S extends don> S save(S entity) {
+        return donDao.save(entity);
+    }
+
+    public void deleteById(Long aLong) {
+        donDao.deleteById(aLong);
+    }
 }
